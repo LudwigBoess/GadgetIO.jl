2225e962f4ce6c0508f60fd573d228dfe83c5eea
Ludwig Böss <ludwig.boess@outlook.de>
2021-01-12 15:52:42 +0100

    rearranged order of functions

--- src/read_snapshot/utility/domain_slices.jl
+++ src/read_snapshot/utility/domain_slices.jl
@@ -1,6 +1,30 @@
 using LinearAlgebra
 
 """
+    filter_cube(snap_file::String, corner_lowerleft::Array{<:Real}, corner_upperright::Array{<:Real}, 
+                          parttype::Integer)
+
+Reads positions from `snap_file` and returns the indices of particles contained in a box defined by `corner_lowerleft` and `corner_upperright`.
+"""
+function filter_cube(snap_file::String, corner_lowerleft::Array{<:Real}, corner_upperright::Array{<:Real}; parttype::Integer=0)
+
+    # read positions from file
+    pos = read_snap(snap_file, "POS", parttype)
+
+    # double check if the corners are set correctly
+    cube_lower = Array{eltype(corner_lowerleft[1]),1}(undef, 3)
+    cube_upper = Array{eltype(corner_lowerleft[1]),1}(undef, 3)
+    @inbounds for dim = 1:3
+        cube_lower[dim] = ((corner_lowerleft[dim] < corner_upperright[dim]) ? corner_lowerleft[dim] : corner_upperright[dim])
+        cube_upper[dim] = ((corner_lowerleft[dim] > corner_upperright[dim]) ? corner_lowerleft[dim] : corner_upperright[dim])
+    end
+
+    return findall( @views ( ( cube_lower[1] .<= pos[1,:] .<= cube_upper[1] ) .& 
+                             ( cube_lower[2] .<= pos[2,:] .<= cube_upper[2] ) .&
+                             ( cube_lower[3] .<= pos[3,:] .<= cube_upper[3] )) )
+end
+
+"""
     check_in_cylinder(x, pt1, pt2, r)
 
 Checks if a 3D point `x` is in a cylinder defined by its endpoints `pt1` and `pt2` and radius `r`.
@@ -34,30 +58,6 @@ function filter_cylinder(filename::String, pt1::Array{<:Real}, pt2::Array{<:Real
     end
 end
 
-
-"""
-    filter_cube(snap_file::String, corner_lowerleft::Array{<:Real}, corner_upperright::Array{<:Real}, 
-                          parttype::Integer)
-
-Reads positions from `snap_file` and returns the indices of particles contained in a box defined by `corner_lowerleft` and `corner_upperright`.
-"""
-function filter_cube(snap_file::String, corner_lowerleft::Array{<:Real}, corner_upperright::Array{<:Real}; parttype::Integer=0)
-
-    # read positions from file
-    pos = read_snap(snap_file, "POS", parttype)
-
-    # double check if the corners are set correctly
-    cube_lower = Array{eltype(corner_lowerleft[1]),1}(undef, 3)
-    cube_upper = Array{eltype(corner_lowerleft[1]),1}(undef, 3)
-    @inbounds for dim = 1:3
-        cube_lower[dim] = ((corner_lowerleft[dim] < corner_upperright[dim]) ? corner_lowerleft[dim] : corner_upperright[dim])
-        cube_upper[dim] = ((corner_lowerleft[dim] > corner_upperright[dim]) ? corner_lowerleft[dim] : corner_upperright[dim])
-    end
-
-    return findall( @views ( ( cube_lower[1] .<= pos[1,:] .<= cube_upper[1] ) .& 
-                             ( cube_lower[2] .<= pos[2,:] .<= cube_upper[2] ) .&
-                             ( cube_lower[3] .<= pos[3,:] .<= cube_upper[3] )) )
-end
 
 
 """
