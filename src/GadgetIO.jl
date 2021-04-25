b5d39d97eb14aee210d47e2d7275011a8d30dcec
LudwigBoess <lboess@usm.lmu.de>
2021-04-25 19:42:37 +0200

    re-arranged inclusion of files for find_read_positions

--- src/GadgetIO.jl
+++ src/GadgetIO.jl
@@ -20,8 +20,6 @@ module GadgetIO
     include(joinpath("read_subfind",  "read_subfind.jl"))
     include(joinpath("read_subfind",  "read_halo_prop.jl"))
     include(joinpath("read_subfind",  "filter_subfind.jl"))
-    include(joinpath("read_snapshot", "distributed_files", "find_read_positions.jl"))
-    include(joinpath("read_snapshot", "distributed_files", "read_distributed_files.jl"))
     include(joinpath("read_snapshot", "particles_in_box",  "read_key_files.jl"))
     include(joinpath("read_snapshot", "particles_in_box",  "peano_hilbert.jl"))
     include(joinpath("read_snapshot", "particles_in_box",  "utility.jl"))
@@ -32,7 +30,9 @@ module GadgetIO
     include(joinpath("read_snapshot", "particles_in_geometry", "read_particles_in_geometry.jl"))
     include(joinpath("read_snapshot", "particles_in_halo", "read_particles_by_id.jl"))
     include(joinpath("read_snapshot", "particles_in_halo", "read_particles_in_halo.jl"))
-
+    include(joinpath("read_snapshot", "distributed_files", "find_read_positions.jl"))
+    include(joinpath("read_snapshot", "distributed_files", "read_distributed_files.jl"))
+    
     # functions to write snapshots
     include(joinpath(dirname(@__FILE__), "write_snapshot", "write_snap.jl"))
 
