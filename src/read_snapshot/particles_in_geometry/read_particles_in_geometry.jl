dcae6a6a5aac41dcbf02f189b5eb280f12fa2d0a
Lucas Valenzuela <lucasvalenzuela@users.noreply.github.com>
2021-04-30 23:37:23 +0200

    Use notin symbol instead of inverse in

--- src/read_snapshot/particles_in_geometry/read_particles_in_geometry.jl
+++ src/read_snapshot/particles_in_geometry/read_particles_in_geometry.jl
@@ -18,7 +18,7 @@ function read_particles_in_geometry(filename::String, blocks::Vector{String},
     h = read_header(filename * ".0")
 
     # make sure position is read - needed for determining if in geometry or not
-    if !("POS" ∈ blocks)
+    if "POS" ∉ blocks
         @info "POS not found in blocks, adding POS..."
         append!(blocks, "POS")
     end
